a52721e90fae94d04d2efa6ef17882802eb16e27
wangsd <shengdong.wsd@alibaba-inc.com>
2022-03-21 16:06:21 +0800

    init

---
+++ src/main/java/io/terminus/debugger/DebuggerApplication.java
@@ -0,0 +1,13 @@
+package io.terminus.debugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebuggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebuggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/terminus/debugger/DebuggerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.terminus.debugger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebuggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
