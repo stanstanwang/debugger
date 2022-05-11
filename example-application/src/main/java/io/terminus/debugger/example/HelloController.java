721a3f0a3d07cc3ea3be5395030c89a529a0c49a
wangsd <shengdong.wsd@alibaba-inc.com>
2022-05-11 12:11:58 +0800

    feature: add log

--- example-application/src/main/java/io/terminus/debugger/example/HelloController.java
+++ example-application/src/main/java/io/terminus/debugger/example/HelloController.java
@@ -1,5 +1,6 @@
 package io.terminus.debugger.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022/5/9
  */
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World", required = false) String name) {
+        log.info("hello world {}", System.currentTimeMillis());
         return String.format("Hello %s!", name);
     }
 }
